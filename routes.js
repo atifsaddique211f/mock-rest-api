bc45e5978672b57e058a129c28491fe88155be69
Atif Saddique <atif@aurorasolutions.io>
2016-10-26 21:16:47 +0500

    Add post request to add asset

--- routes.js
+++ routes.js
@@ -1,12 +1,25 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var fixtures;
+var maxId = 5;
 
 var getFixture = function(fixtureName) {
-  return require('./fixtures/' + fixtureName + '.json')
+  if(fixtures) {
+    return fixtures;
+  }
+  fixtures = require('./fixtures/' + fixtureName + '.json');
+  return fixtures;
 };
 
 router.get('/api/v1/assets', function(req, res) {
   res.send(getFixture('asset-list'));
+});
+
+router.post('/api/v1/asset', function(req, res) {
+  req.body.assetId = ++maxId;
+  fixtures.push(req.body);
+  res.send(req.body);
 });
 
 
