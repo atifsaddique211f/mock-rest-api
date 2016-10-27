54ac4b2d58cb4f9d69cbefd97a785dd7553720f0
Atif Saddique <atif@aurorasolutions.io>
2016-10-27 15:35:49 +0500

    Add delete and update asset request

--- routes.js
+++ routes.js
@@ -16,8 +16,33 @@ router.get('/api/v1/assets', function(req, res) {
   res.send(getFixture('asset-list'));
 });
 
+router.get('/api/v1/asset/:id', function(req, res) {
+  getFixture('asset-list');
+  var fixture = fixtures.filter(function(item) {
+    if(item.assetId == req.params.id)return item;
+  });
+  res.send(fixture[0]);
+});
+
+router.delete('/api/v1/asset/:id', function(req, res) {
+  getFixture('asset-list');
+  var fixture = fixtures.filter(function(item) {
+    if(item.assetId == req.params.id)return item;
+  });
+  fixtures.splice(fixtures.indexOf(fixture[0]),1);
+  res.send(fixture[0]);
+});
+
 router.post('/api/v1/asset', function(req, res) {
-  req.body.assetId = ++maxId;
+  if(req.body.assetId){
+    var fixture = fixtures.filter(function(item) {
+      if(item.assetId == req.body.assetId)return item;
+    });
+    fixtures.splice(fixtures.indexOf(fixture[0]),1);
+  }
+  else {
+    req.body.assetId = ++maxId;
+  }
   fixtures.push(req.body);
   res.send(req.body);
 });
